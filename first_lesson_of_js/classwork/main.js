5aae96beadd9fb5e92507dd4930e7eaf920e3084
Vlados <vladislav25012008@gmail.com>
2022-03-08 12:36:10 +0200

    new file

---
+++ first_lesson_of_js/classwork/main.js
@@ -0,0 +1,29 @@
+let massive = [ 1 + 2 + 3+ 4 + 5 + 6 + 7 + 8 + 9 + 10];
+let result = massive;
+console.log(result)
+let book1 = {
+    name: 'Harry Potter',
+    quantity_of_pages: 350,
+    genre: 'fantasy',
+    author: 'Joan Katling Roling'
+}
+let book2 = {
+    name: 'Sherlock Holmes',
+    quantity_of_pages: 250,
+    genre: 'detective',
+    author: 'Arthur Conan Doyle'
+}
+let width = 23;
+let height = 10;
+let s = width * height;
+console.log(s)
+let heighC = 10;
+let dC = 4;
+let p = 3.14;
+let v = p * 2**2 * heighC ;
+console.log( v);
+let n = 3;
+let m = 4;
+let k = n + n + m;
+
+console.log(k)
---
+++ first_lesson_of_js/homework/main.js
@@ -0,0 +1,59 @@
+"use strict";
+let one = 'hello';
+let two = 'owu';
+let three = 'com';
+let four = 'ua';
+let five = 1;
+let six = 10;
+let seven = -999;
+let eight = 123;
+let nine = 3.14;
+let ten = 2.7;
+let eleven = 16;
+
+console.log(one);
+console.log(two);
+console.log(three);
+alert(four);
+alert(eight);
+alert(nine);
+console.log(five);
+console.log(six);
+
+let firstname = prompt('твоє імя')
+let lastname = prompt('твій вік');
+console.log('вітаю' + ' ' + firstname + ' ' + 'тобі' + ' ' + lastname)
+
+let a = 100;
+let b = 'hello';
+let c = true;
+console.log( typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+
+console.log(true);
+console.log(false);
+let first1 = 5 < 6;
+console.log(first1);
+let second1 = 5 > 6;
+console.log(second1);
+let third1 = 5 == 6;
+console.log(third1);
+let fourth1 = 5 >= 6;
+console.log(fourth1);
+let first2 = 10 == 10;
+console.log(first2)
+let second2 = 10 === 10;
+console.log(second2)
+let third2 = 10 > 10;
+console.log(third2)
+let fourth2 = 10 < 10;
+console.log(fourth2)
+let fifth2 = 10 >= 10;
+console.log(fifth2)
+let first3 = 123 > 123;
+console.log(first3)
+let second3 = 123 == 123;
+console.log(second3)
+
