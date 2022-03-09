c2f1b45f73fe4e0c6cfa18a02a743c0777932041
Vlados <vladislav25012008@gmail.com>
2022-03-09 12:45:51 +0200

    new file

--- first_lesson_of_js/homework/main.js
+++ first_lesson_of_js/homework/main.js
@@ -1,4 +1,4 @@
-"use strict";
+//Створити змінні. Присвоїти кожному з них значення:
 let one = 'hello';
 let two = 'owu';
 let three = 'com';
@@ -10,7 +10,7 @@ let eight = 123;
 let nine = 3.14;
 let ten = 2.7;
 let eleven = 16;
-
+//Вивести кожну змінну за допомогою: console.log , alert, document.write
 console.log(one);
 console.log(two);
 console.log(three);
@@ -20,10 +20,14 @@ alert(nine);
 console.log(five);
 console.log(six);
 
+//-Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
 let firstname = prompt('твоє імя')
 let lastname = prompt('твій вік');
+// Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
 console.log('вітаю' + ' ' + firstname + ' ' + 'тобі' + ' ' + lastname)
 
+//За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
 let a = 100;
 let b = 'hello';
 let c = true;
@@ -31,7 +35,8 @@ console.log( typeof a);
 console.log(typeof b);
 console.log(typeof c);
 
-
+//Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+// В однакових виразах не використовувати однакові оператори!!!
 console.log(true);
 console.log(false);
 let first1 = 5 < 6;
